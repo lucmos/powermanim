e7c8524e8da1e62159f872bd65506d257618a3ee
Luca Moschella <luca.moschella94@gmail.com>
2023-03-20 00:08:55 +0100

    Add script to create animation pages in the website

--- src/powermanim/showcase/__init__.py
+++ src/powermanim/showcase/__init__.py
@@ -2,11 +2,16 @@ import importlib
 import sys
 from pathlib import Path
 
+# Import all showcased scenes, in order for them to be registered in ShowcaseMeta
+
 all_python_files = [
     f for f in Path(__file__).parent.rglob("*.py") if f.is_file() and not f.name.endswith("__init__.py")
 ]
 for m in all_python_files:
-    spec = importlib.util.spec_from_file_location(str(m.with_suffix("")), m)
+    # TODO: fix this hack to get the module name
+    module_name = ".".join(str(m.with_suffix("").relative_to(Path(__file__).parent.parent)).split("/"))
+
+    spec = importlib.util.spec_from_file_location(module_name, m)
     foo = importlib.util.module_from_spec(spec)
     sys.modules["module.name"] = foo
     spec.loader.exec_module(foo)
--- tests/showcase/test_showcasing.py
+++ tests/showcase/test_showcasing.py
@@ -1,16 +1,13 @@
 from manim import *
 
-from powermanim.showcase.showcasescene import ShowcaseMeta
-
-WOWCHEMY_FOLDER = "docs/static/uploads/media"
-SHOWCASE_QUALITY = "medium_quality"
+from powermanim.showcase.showcasescene import QUALITY, WOWCHEMY_FOLDER, ShowcaseMeta
 
 
 def test_and_gen_showcases():
     for scene in ShowcaseMeta.showcase_scenes:
         with tempconfig(
             {
-                "quality": SHOWCASE_QUALITY,
+                "quality": QUALITY,
                 "disable_caching": True,
                 "custom_folders": True,
                 "media_dir": WOWCHEMY_FOLDER,
