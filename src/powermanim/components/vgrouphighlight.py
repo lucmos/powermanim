c1eee6501d785e73739c150ca131831744fbdbaf
Luca Moschella <luca.moschella94@gmail.com>
2023-03-22 11:28:39 +0100

    Update docs

--- src/powermanim/components/vgrouphighlight.py
+++ src/powermanim/components/vgrouphighlight.py
@@ -42,6 +42,17 @@ class VGroupHighlight(VGroup):
         self.previously_active_idxs = []
 
     def highlight(self, scene: Scene, indices: T.Union[int, T.Sequence[int]]) -> None:
+        """Highlights the submobjects in the given indices in the scene.
+
+        Args:
+            scene:
+                The scene in which the animation is played.
+
+            indices:
+                The indices to highlight. If a single integer is given, only that index is highlighted.
+                If a sequence of integers is given, all indices in the sequence are highlighted. The
+                previously highlighted indices are dehighlighted smoothly.
+        """
         anims = []
 
         if not isinstance(indices, T.Sequence):
