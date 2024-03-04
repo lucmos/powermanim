3cdcc2cc5596b8b9a2f295bc16ba3f9c21ef2c6c
Luca Moschella <luca.moschella94@gmail.com>
2024-03-04 08:02:03 +0100

    Add reference component

---
+++ src/powermanim/showcase/templates/reference.py
@@ -0,0 +1,24 @@
+from manim import *
+
+from powermanim.showcase.showcasescene import ShowcaseScene
+from powermanim.templates.reference import Reference
+
+
+class SectionTitleShowcase(ShowcaseScene):
+    def showcasing():
+        return Reference
+
+    def construct(self):
+        text = (
+            r"Moschella et al. “Relative representations enable zero-shot latent space communication”, ICLR 2023 (oral)"
+        )
+        tex = Tex(text).scale(0.5)
+        reference = Reference(text=text)
+
+        self.add(tex)
+
+        self.play(
+            Transform(tex, reference[0]),
+            rate_func=there_and_back_with_pause,
+            run_time=3,
+        )
---
+++ src/powermanim/templates/reference.py
@@ -0,0 +1,15 @@
+from manim import *
+
+
+class Reference(VGroup):
+
+    def __init__(self, text: str, font_size: int = 28, color: ManimColor = GRAY):
+
+        text = rf"\textit{{{text}}}"
+        boxtext = rf"\mbox{{{text}}}"
+        tex = Tex(
+            boxtext,
+            font_size=font_size,
+            color=color,
+        ).to_corner(DL, buff=MED_SMALL_BUFF)
+        super().__init__(tex)
