21dfeb9c149a057b1ceb187449c63cb5a1fed4d4
Luca Moschella <luca.moschella94@gmail.com>
2024-04-04 00:22:02 +0200

    Fix inconsistency in showcasing spacing

--- src/powermanim/showcase/layouts/arrangedbullets.py
+++ src/powermanim/showcase/layouts/arrangedbullets.py
@@ -19,14 +19,10 @@ class ArrangedBulletsShowcase(ShowcaseScene):
             Bullet("Third element", level=1, symbol="(3)"),
         ]
         g_rows = VGroup(*rows).set_opacity(0.25).scale(0.9)
-        g_rows.target = (
-            ArrangedBullets(
-                *g_rows.copy(),
-                line_spacing_decay=0.65,
-            )
-            .to_edge(LEFT, buff=MED_LARGE_BUFF * 3)
-            .set_opacity(1)
-        )
+        g_rows.target = ArrangedBullets(
+            *g_rows.copy(),
+            line_spacing_decay=0.65,
+        ).set_opacity(1)
 
         self.play(
             MoveToTarget(g_rows),
