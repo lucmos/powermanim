f679523ac79c6af16c1eb1a0a20595887a5a93e1
Luca Moschella <luca.moschella94@gmail.com>
2024-04-03 18:54:12 +0200

    Implement spacing decaying at increasing levels

--- src/powermanim/templates/bulletlist.py
+++ src/powermanim/templates/bulletlist.py
@@ -10,9 +10,8 @@ class BulletList(GroupActivable):
         self,
         *rows: T.Union[MathBullet, Tex, Text],
         line_spacing: float = MED_LARGE_BUFF * 1.5,
+        line_spacing_decay: float = 1.0,  # TODO: in the next version, change default to 0.5
         indent_buff: float = MED_LARGE_BUFF * 1.5,
-        left_buff: float = MED_LARGE_BUFF * 2,
-        global_shift: float = 0.0,
         inactive_opacity: float = 0.5,
         active_opacity: float = 1.0,
         scale_active: float = 1.0,
@@ -23,9 +22,8 @@ class BulletList(GroupActivable):
         Args:
             rows: A list of items to be displayed.
             line_spacing: The spacing between the rows.
+            line_spacing_decay: The rate at which the spacing decays at each level
             indent_buff: The spacing between the bullet and the text.
-            left_buff: The spacing between the left edge of the rows and the left edge of the screen.
-            global_shift: The global_shift to apply to the rows.
             inactive_opacity: The opacity of the inactive items.
             active_opacity: The opacity of the active items.
             scale_active: The scale of the active items.
@@ -34,9 +32,8 @@ class BulletList(GroupActivable):
         self.arranged_list = ArrangedBullets(
             *rows,
             line_spacing=line_spacing,
+            line_spacing_decay=line_spacing_decay,
             indent_buff=indent_buff,
-            left_buff=left_buff,
-            global_shift=global_shift,
         )
 
         super().__init__(
